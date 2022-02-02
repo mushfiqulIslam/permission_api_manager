f9ac25662ceb674432bda12db309cb689c18faf5
mushfiqulIslam <nnahid878@gmail.com>
2022-02-02 23:05:51 +0600

    building sturcture

---
+++ config/config.go
@@ -0,0 +1,14 @@
+package config
+
+// Structure of Config file
+//type ConfYaml struct {
+//	Core    SectionCore    `yaml:"core"`
+//	API     SectionAPI     `yaml:"api"`
+//	Android SectionAndroid `yaml:"android"`
+//	Huawei  SectionHuawei  `yaml:"huawei"`
+//	Ios     SectionIos     `yaml:"ios"`
+//	Queue   SectionQueue   `yaml:"queue"`
+//	Log     SectionLog     `yaml:"log"`
+//	Stat    SectionStat    `yaml:"stat"`
+//	GRPC    SectionGRPC    `yaml:"grpc"`
+//}
---
+++ router/server.go
@@ -0,0 +1,9 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func routerEngine(cfg *config.ConfYaml, q *queue.Queue) *gin.Engine {
+
+}
